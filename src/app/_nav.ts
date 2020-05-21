daa78ceeac673403ad7bd2d13d1ad3ddf20e24b4
vinothtrigyn <vinoth.moorthy@trigyn.com>
2020-05-21 11:08:12 +0530

    first commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,10 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+
+  {
+    name: 'Users',
+    url: '/user',
+    icon: 'icon-speedometer'
+  }
+];
---
+++ src/app/app.routing.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import { DefaultLayoutComponent } from './containers';
+
+import { P404Component } from './views/error/404.component';
+import { P500Component } from './views/error/500.component';
+import { LoginComponent } from './views/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'user',
+    pathMatch: 'full',
+  },
+  {
+    path: '404',
+    component: P404Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: P500Component,
+    data: {
+      title: 'Page 500'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'user',
+        loadChildren: () => import('./views/users/users.module').then(m => m.UsersModule)
+      }
+    ]
+  },
+  { path: '**', component: P404Component }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
